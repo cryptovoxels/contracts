bd64dc07ed6a0346d4688e53088c6902053deb53
Ben Nolan <bnolan@gmail.com>
2018-06-05 20:10:40 +1200

    Remove unused function

--- contracts/Parcel.sol
+++ contracts/Parcel.sol
@@ -8,22 +8,6 @@ library String {
     From https://ethereum.stackexchange.com/questions/10811/solidity-concatenate-uint-into-a-string
   */
 
-  function uintToString(uint v) internal pure returns (string str) {
-      uint maxlength = 100;
-      bytes memory reversed = new bytes(maxlength);
-      uint i = 0;
-      while (v != 0) {
-          uint remainder = v % 10;
-          v = v / 10;
-          reversed[i++] = byte(48 + remainder);
-      }
-      bytes memory s = new bytes(i);
-      for (uint j = 0; j < i; j++) {
-          s[j] = reversed[i - 1 - j];
-      }
-      str = string(s);
-  }
-
   function appendUintToString(string inStr, uint v) internal pure returns (string str) {
       uint maxlength = 100;
       bytes memory reversed = new bytes(maxlength);
